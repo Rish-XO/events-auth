b2fc925bae89fb2d7f5b2206c25e091473fed39c
Rishal <Rishal.mhd009@gmail.com>
2023-06-03 17:18:14 +0530

    route protection

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,8 +1,20 @@
+import { redirect } from "react-router-dom";
+
 export function getAuthtoken() {
-    const token = localStorage.getItem('token');
-    return token;
+  const token = localStorage.getItem("token");
+  return token;
 }
 
 export function tokenLoader() {
-    return getAuthtoken();
+  return getAuthtoken();
+}
+
+export function checkAuthLoader() {
+  const token = getAuthtoken();
+
+  if (!token) {
+    return redirect("/auth");
+  }
+
+  return null;
 }
